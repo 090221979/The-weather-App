1d759d50e1c8cfeaf38de31d866e8cb99ac5b8a2
090221979 <69111078+090221979@users.noreply.github.com>
2026-07-03 00:32:08 +0100

    Refactor fetchWeatherData to use environment variable
    updated the API key information as it was visible

--- vitejs-vite-bp4ccwwp (1)/src/utils/fetchWeatherData.js
+++ vitejs-vite-bp4ccwwp (1)/src/utils/fetchWeatherData.js
@@ -1,11 +1,11 @@
-const API_KEY = 'f38be283ee5cb82181d5149e5fb83154';
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export async function fetchWeatherApp(cityName) {
   if (!cityName || cityName.trim() === '') return null;
 
   const response = await fetch(
-    `${API_URL}?q=${cityName}&appid=${API_KEY}&units=imperial`
+    `${API_URL}?q=${cityName}&appid=${apiKey}&units=imperial`
   );
 
   if (!response.ok) {
